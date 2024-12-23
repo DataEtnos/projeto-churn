4c80346a939f5f75f4e6186c9537cf97930f43c2
Etnos De Jesus Santos <128651513+DataEtnos@users.noreply.github.com>
2024-12-23 17:13:35 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+
+opcao = st.sidebar.header('Introdução')
+
+st.title('Modelo de Previsão de Churn')
+
+st.markdown("Este Aplicativo tem como objetivo fazer previsões de churn do aplicativo de Música, foi treinado por Modelo Básico de Machine Learning para gerar Previsões de Churn,")
+
+st.write("\n\n")
+
+
+st.success('Por favor siga para as proximas paginas para previsões do modelo')
